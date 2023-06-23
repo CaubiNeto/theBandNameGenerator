ecec856746d0e933aad20fa7c33b39516e6b5098
Caubi <caubineto@outlook.com>
2023-06-23 12:12:11 -0300

    segundo commit

---
+++ project1.py
@@ -0,0 +1,13 @@
+# criei a saudação do programa. print simples
+print("Welcome to the Band Name Generator.")
+
+# perguntei qual o nome da city com input
+# atibuindo o result a variavel cityName
+cityName = input('Whats the name of the city you grew up?\n ')
+
+# perguntei p nome do pet com input
+# atribuindo a variavel petName
+petName = input('Whats your pet name?\n ')
+
+# combinei o nome da cidade com o pet com concatenação
+print('Your band name could be ' + cityName +' '+ petName)
